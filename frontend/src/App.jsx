d37a2d20a8947042282093e785a58fd71fcb0610
Priyanshu sharma <priyanshus8282@gmail.com>
2025-01-11 14:26:15 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import {  HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -12,8 +12,9 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <div>
       <Navbar />
+      
     <Routes>
      <Route path="/" element={<Home/>} />
      <Route path="/about" element={<About/>} />
@@ -24,8 +25,9 @@ function App() {
        
      
     </Routes>
+
     <Footer/>
-    </Router>
+    </div>
   );
 }
 
